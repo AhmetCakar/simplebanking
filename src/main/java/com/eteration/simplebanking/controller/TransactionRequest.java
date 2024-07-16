aeab1194c13e6c11a0a7f57fe8428fc93b91b925
CakarAhmet <cakar.ce@hotmail.com>
2024-07-16 07:57:46 +0300

    first commit

---
+++ src/main/java/com/eteration/simplebanking/controller/TransactionRequest.java
@@ -0,0 +1,20 @@
+package com.eteration.simplebanking.controller;
+
+public class TransactionRequest {
+    private double amount;
+
+    public TransactionRequest() {
+    }
+
+    public TransactionRequest(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
--- src/main/java/com/eteration/simplebanking/model/Account.java
+++ src/main/java/com/eteration/simplebanking/model/Account.java
@@ -1,7 +1,50 @@
 package com.eteration.simplebanking.model;
+import lombok.Data;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
-
-// This class is a place holder you can change the complete implementation
-
+@Data
+@Entity
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String owner;
+    private String accountNumber;
+    private double balance;
+
+    @OneToMany(mappedBy = "bankAccount", cascade = CascadeType.ALL)
+    private List<Transaction> transactions = new ArrayList<>();
+
+    public Account() {}
+
+    public Account(String owner, String accountNumber) {
+        this.owner = owner;
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+    }
+
+    public void deposit(double amount) {
+        DepositTransaction transaction = new DepositTransaction(amount);
+        post(transaction);
+    }
+
+    public void withdraw(double amount) throws InsufficientBalanceException {
+        WithdrawalTransaction transaction = new WithdrawalTransaction(amount);
+        post(transaction);
+    }
+
+    public void post(Transaction transaction) throws InsufficientBalanceException {
+        if (transaction instanceof DepositTransaction) {
+            balance += transaction.getAmount();
+        } else if (transaction instanceof WithdrawalTransaction) {
+            if (balance >= transaction.getAmount()) {
+                balance -= transaction.getAmount();
+            } else {
+                throw new InsufficientBalanceException("Insufficient balance");
+            }
+        }
+        transactions.add(transaction);
+    }
 }
